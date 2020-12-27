ac4774428dc3bfa77a7d21dc4508f3a65a832991
Radish4649 <69351166+Radish4649@users.noreply.github.com>
2020-12-27 13:33:09 +0900

    improved cipher

--- projects/Cipher/script.js
+++ projects/Cipher/script.js
@@ -1,16 +1,46 @@
-document.getElementById("button").addEventListener("click", translate)
+document.getElementById("encode-button").addEventListener("click", encode)
+document.getElementById("decode-button").addEventListener("click", decode)
 
-function translate() {
+// -- full functions --
+
+function encode() {
     var input = document.getElementById("input").value
-    var output = cipher(input, 13)
+    var output = edCipher(input, true)
     document.getElementById("output").value = output
 }
 
+function decode() {
+    var input = document.getElementById("input").value
+    var output = edCipher(input, false)
+    document.getElementById("output").value = output
+}
 
+// -- helper functions --
 
+function edCipher(input, direction) {
+    str = input.toLowerCase().split("").filter(x=>/[A-Za-z]/.test(x))
+    for (let i = 0; i < str.length; i++) {
+        if (i%2) {
+            if (direction) {
+                str[i] = cipher(str[i], letterToNumber(str[i-1]))
+            } else {
+                str[i] = cipher(str[i], 26 - letterToNumber(str[i-1]))
+            }
+        }
+    }
+    return str.join("")
+}
+
+// ciphers each individual letter
 function cipher(s, k) {
     return s.replace(/[a-z]/gi, m => {
       const c = m < "a" ? 65 : 97;
       return String.fromCharCode((m.charCodeAt(0) - c + k) % 26 + c)
     });
   }
+
+
+// gets the number for each letter
+function letterToNumber(letter) {
+    return letter.charCodeAt(0) - 96
+}
